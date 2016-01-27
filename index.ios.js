4a3d97348aefb1a8f5605dce2194b11c9d72b9ed
pbryzek <bryzek@gmail.com>
2016-01-27 02:22:45 -0700

    enable current location data and send to server

--- index.ios.js
+++ index.ios.js
@@ -18,8 +18,64 @@ var TabTwo = require('./js/tab_two.js');
 
 var style = require('./style/style.js');
 var globals = require('./js/constants/globals.js');
+var PARAMs = require('./js/constants/constants_params.js');
+var APIs = require('./js/constants/constants_api.js');
+var GLOBALs = require('./js/constants/globals.js');
 
 class Kitchit extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {count: props.initialCount};
+    this.watchID = null;
+  }
+
+  sendLocationToServer(position) {
+      console.log("position = " + JSON.stringify(position));
+      //var initialPosition = JSON.stringify(position);
+
+        //TODO get the userId correctly
+        var userId = 1;
+        var coords = position.coords;
+        var latitude = coords.latitude;
+        var longitude = coords.longitude;
+
+        var params = {};
+        params[PARAMs.USERID] = userId;
+        params[PARAMs.LATITUDE] = latitude;
+        params[PARAMs.LONGITUDE] = longitude;
+
+        var acceptPath = GLOBALs.createUrl(APIs.UPDATE_CHEF_LOCATION, params);
+
+        fetch(acceptPath)
+        .then((response) => response.json())
+        .then((responseData) => {
+                var success = responseData[PARAMs.SUCCESS];
+                if(success) {
+                } else {
+                        var errMsg = responseData[PARAMs.ERRORMSG];
+			console.log("Error sending location data " + errMsg);
+                }
+        })
+        .catch((error) => {
+                console.warn(error);
+        });
+  }
+
+  componentDidMount() { 
+      navigator.geolocation.getCurrentPosition( (position) => { 
+          this.sendLocationToServer(position);          
+      }, (error) => alert(error.message), 
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000} ); 
+      this.watchID = navigator.geolocation.watchPosition((position) => { 
+          this.sendLocationToServer(position);
+      }); 
+   }
+
+  componentWillUnmount() { 
+      navigator.geolocation.clearWatch(this.watchID); 
+  }
+
   render() {
     return (
       <TabBarNavigator
